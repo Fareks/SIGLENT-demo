a761c7ab91c44c3b4bb2fcb7508abe64ad9c96ac
Fareks <mselinkor@gmail.com>
2022-06-09 20:34:40 +0300

    fix bugs

--- src/components/main-section/main-section.js
+++ src/components/main-section/main-section.js
@@ -8,9 +8,9 @@ const MainSection = () => {
             <div className="poster-and-main-text">
                 <div className="poster-area">
                     <motion.img 
-                    initial={{opacity:0, x:-30}}
-                    animate={{opacity:1,x:0}}
-                    transition={{delay:0.1,duration:0.3, ease:"easeOut"}}
+                    // initial={{opacity:0}}
+                    // animate={{opacity:1}}
+                    // transition={{delay:0.1,duration:0.3, ease:"easeOut"}}
                     className="main-poster"
                     src={main_poster} 
                     alt="alternatetext" 
@@ -23,13 +23,13 @@ const MainSection = () => {
 
                 className="main-text-area">
                     <motion.h1
-                    initial={{opacity:0, x:-30}}
-                    animate={{opacity:1,x:0}}
+                    initial={{opacity:0}}
+                    animate={{opacity:1}}
                     transition={{delay:0.3,duration:0.3, ease:"easeOut"}}
                     > ОФІЦІЙНИЙ ДИСТРИБ’ЮТОР БРЕНДУ SIGLENT</motion.h1>
                     <motion.p
-                    initial={{opacity:0, x:-30}}
-                    animate={{opacity:1,x:0}}
+                    initial={{opacity:0}}
+                    animate={{opacity:1}}
                     transition={{delay:0.5,duration:0.3, ease:"easeOut"}}
                     className="main-text-secondary"
                     >
