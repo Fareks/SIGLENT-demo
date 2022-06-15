1d905a8382edc81d4e449cf139d4dacbc114313b
Fareks <mselinkor@gmail.com>
2022-06-15 10:44:03 +0300

    contacts update

--- src/components/pages/Contacts-components/Contacts.js
+++ src/components/pages/Contacts-components/Contacts.js
@@ -1,53 +1,7 @@
 import Label from '../../other-projects-section/label'
 import "./contacts.css"
 import { useState,useCallback } from 'react';
-import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
-
-const containerStyle = {
-  width: '400px',
-  height: '400px'
-};
-
-const center = {
-  lat: -3.745,
-  lng: -38.523
-};
-
-function GoogleMapCom() {
-  const { isLoaded } = useJsApiLoader({
-    id: 'google-map-script',
-    googleMapsApiKey: "YOUR_API_KEY"
-  })
-
-  const [map, setMap] = useState(null)
-
-  const onLoad = useCallback(function callback(map) {
-    const bounds = new window.google.maps.LatLngBounds(center);
-    map.fitBounds(bounds);
-    setMap(map)
-  }, [])
-
-  const onUnmount = useCallback(function callback(map) {
-    setMap(null)
-  }, [])
-
-  return isLoaded ? (
-      <GoogleMap
-        mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
-        onLoad={onLoad}
-        onUnmount={onUnmount}
-      >
-        { /* Child components, such as markers, info windows, etc. */ }
-        <></>
-      </GoogleMap>
-  ) : <></>
-}
-
-
-
-
+import mapImg from "../../../img-content/map.png"
 
 const Contacts = () => {
 return (
@@ -93,9 +47,9 @@ return (
                 </div>
             </div>
         </div>
-        {/* <div className="map-main-container">
-        <GoogleMapCom/>
-        </div> */}
+        <div className="map-main-container">
+            <img src={mapImg} alt="" className='mapImg'/>
+        </div>
      </div>
  </div>
 );
